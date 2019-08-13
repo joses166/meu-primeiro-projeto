e5f701c2ce780eda30640db92e6a3e1a3406ce4c
unknown <ton.ton.leite@hotmail.com>
2019-08-13 10:40:52 -0300

    Versão inicial do projeto.

---
+++ src/main/java/br/com/mrl/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
